65661f1d37176177598f54b1af0eecbf49d6b648
Nhaan2003 <hoainhan2904@gmai.com>
2025-05-21 23:53:56 +0700

    cập nhật 3

--- adminMovieBooking-master/DemoAdmin/src/main/java/com/example/DemoAdmin/entity/BookingSeat.java
+++ adminMovieBooking-master/DemoAdmin/src/main/java/com/example/DemoAdmin/entity/BookingSeat.java
@@ -1,23 +1,23 @@
-//package com.example.DemoAdmin.entity;
-//
-//import jakarta.persistence.*;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//@Entity
-//@IdClass(BookingSeatId.class)
-//@Table(name = "BookingSeat")
-//public class BookingSeat {
-//
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "BookingId")
-//    private Booking booking;
-//
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "SeatId")
-//    private Seat seat;
-//}
+package com.example.DemoAdmin.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@IdClass(BookingSeatId.class)
+@Table(name = "BookingSeat")
+public class BookingSeat {
+
+   @Id
+   @ManyToOne
+   @JoinColumn(name = "BookingId")
+   private Booking booking;
+
+   @Id
+   @ManyToOne
+   @JoinColumn(name = "SeatId")
+   private Seat seat;
+}
